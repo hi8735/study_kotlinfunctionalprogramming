f5560135e492f5673fa429d3f3ac1df4de00e5f5
Woo Young Noh <hi8735@gmail.com>
2023-10-11 21:16:52 +0900

    노우영 과제

--- 3장/노우영.kt
+++ 3장/노우영.kt
@@ -1,12 +1,5 @@
 package chapter3
 
-fun main() {
-    val list = List.of(1.1, 0.0, 2.2, 3.3, 4.4, 5.5)
-    val fr = foldRight(list, 0.0, { a: Double, b: Double -> a + b })
-
-    println(fr)
-}
-
 //3.1
 fun <A> tail(xs: List<A>): List<A> = when (xs) {
     is Nil -> throw IllegalStateException("tail called on empty list")
@@ -123,15 +116,91 @@ fun <A, B> map(list: List<A>, f: (A) -> B): List<B> = foldRight(list, List.of())
 
 //3.18
 fun <A> filter(xs: List<A>, f: (A) -> Boolean): List<A> = foldRight(xs, List.of()) {
-    a: A, list: List<A> -> if(f(a)) Cons(a, list) else list
+        a: A, list: List<A> -> if(f(a)) Cons(a, list) else list
 }
 
 //3.19
 fun <A, B> flatMap(xa: List<A>, f:(A) -> List<B>): List<B> = foldRight(xa, List.of()) {a, b -> append(f(a), b)}
 
 // 3.20
-fun <A> filterByFlatMap(xs: List<A>, f: (A) -> Boolean): List<A> =
+fun <A> filterByFlatMap(xs: List<A>, f: (A) -> Boolean): List<A> = flatMap(xs) {if(f(it)) List.of(it) else empty() }
 
+//3.21
+fun addLists(a1: List<Int>, a2: List<Int>): List<Int> = when (a1) {
+    is Nil -> Nil
+    is Cons -> when (a2) {
+        is Nil -> Nil
+        is Cons -> Cons(a1.head + a2.head, addLists(a1.tail, a2.tail))
+    }
+}
+
+//3.22
+fun <A> zipWith(a: List<A>, b: List<A>, f: (A, A) -> A): List<A> = when(a) {
+    is Nil -> empty()
+    is Cons -> when(b) {
+        is Nil -> empty()
+        is Cons -> Cons(f(a.head, b.head), zipWith(a.tail, b.tail, f))
+    }
+}
+
+//3.23
+tailrec fun <A> hasSubsequence(l: List<A>, sub: List<A>): Boolean = when(l) {
+    is Nil -> false
+    is Cons -> if(startsWith(l, sub)) true else hasSubsequence(l.tail, sub)
+}
+
+//3.24
+fun <A> size(tree: Tree<A>): Int = when(tree) {
+    is Leaf -> 1
+    is Branch -> 1 + size(tree.left) + size(tree.right)
+}
+
+//3.25
+fun maximum(tree: Tree<Int>): Int = when(tree) {
+    is Leaf -> tree.value
+    is Branch -> maxOf(maximum(tree.left), maximum(tree.right))
+}
+
+//3.26
+fun depth(tree: Tree<Int>): Int = when(tree) {
+    is Leaf -> 1
+    is Branch -> 1 + maxOf(depth(tree.left), depth(tree.right))
+}
+
+//3.27
+fun <A, B> map(tree: Tree<A>, f: (A) -> B): Tree<B> = when(tree) {
+    is Leaf -> Leaf(f(tree.value))
+    is Branch -> Branch(map(tree.left, f), map(tree.right, f))
+}
+
+//3.28
+fun <A, B> fold(ta: Tree<A>, l: (A) -> B, f: (B, B) -> B): B =
+    when (ta) {
+        is Leaf -> l(ta.value)
+        is Branch -> f(fold(ta.left, l, f), fold(ta.right, l, f))
+    }
+fun <A> sizeF(t: Tree<A>): Int = fold(t, { 1 }, { a, b -> 1 + a + b })
+fun maximumF(t: Tree<Int>): Int = fold(t, { it }, { a, b -> maxOf(a, b) })
+fun <A> depthF(t: Tree<A>): Int = fold(t, { 1 }, { a, b -> 1 + maxOf(a, b) })
+fun <A, B> mapF(ta: Tree<A>, f: (A) -> B): Tree<B> =
+    fold(ta, { a -> Leaf(f(a)) }, { a: Tree<B>, b: Tree<B> -> Branch(a, b) })
+
+sealed class Tree<out A>
+
+data class Leaf<out A>(val value: A) : Tree<A>()
+
+data class Branch<A>(val left: Tree<A>, val right: Tree<A>) : Tree<A>()
+
+
+
+
+fun <A> startsWith(l: List<A>, prefix: List<A>): Boolean = when(l) {
+    is Nil -> false
+    is Cons -> when(prefix) {
+        is Nil -> true
+        is Cons -> if(l.head == prefix.head) startsWith(l.tail, prefix.tail) else false
+    }
+}
 
 fun <A> empty(): List<A> = Nil
 sealed class List<out A> {
